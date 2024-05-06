f7b138dc60e4b1d91c5ac9f686f79375b4f32201
hloba-ukraine <138844087+hloba-ukraine@users.noreply.github.com>
2024-05-05 21:03:00 -0600

    4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,49 +6,56 @@ import LoginPage from "../pages/LoginPage";
 import ContactsPage from "../pages/ContactsPage";
 import Layout from "./ Layout/ Layout";
 import Loader from "./Loader/Loader";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { refreshUser } from "../redux/auth/operations";
 import RestrictedRoute from "./RestrictedRoute/RestrictedRoute";
 import PrivateRoute from "./PrivateRoute/PrivateRoute";
+import { selectIsLoading } from "../redux/auth/selectors";
+
 export default function App() {
+  const isLoading = useSelector(selectIsLoading);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(refreshUser());
   }, [dispatch]);
   return (
     <div>
-      <Layout>
-        <Suspense fallback={<Loader />}>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route
-              path="/register"
-              element={
-                <RestrictedRoute>
-                  <RegistrationPage />
-                </RestrictedRoute>
-              }
-            />
-            <Route
-              path="/login"
-              element={
-                <RestrictedRoute>
-                  <LoginPage />
-                </RestrictedRoute>
-              }
-            />
-            <Route
-              path="/contacts"
-              element={
-                <PrivateRoute>
-                  <ContactsPage />
-                </PrivateRoute>
-              }
-            />
-            {/* <Route path="*" element={<NotFoundPage />} /> */}
-          </Routes>
-        </Suspense>
-      </Layout>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <Layout>
+          <Suspense fallback={<Loader />}>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route
+                path="/register"
+                element={
+                  <RestrictedRoute>
+                    <RegistrationPage />
+                  </RestrictedRoute>
+                }
+              />
+              <Route
+                path="/login"
+                element={
+                  <RestrictedRoute>
+                    <LoginPage />
+                  </RestrictedRoute>
+                }
+              />
+              <Route
+                path="/contacts"
+                element={
+                  <PrivateRoute>
+                    <ContactsPage />
+                  </PrivateRoute>
+                }
+              />
+              {/* <Route path="*" element={<NotFoundPage />} /> */}
+            </Routes>
+          </Suspense>
+        </Layout>
+      )}
     </div>
   );
 }
